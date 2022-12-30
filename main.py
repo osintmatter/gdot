2b577599f92993d6e93ae2ee62865fa09669b2f9
osintmatter <104920726+osintmatter@users.noreply.github.com>
2022-12-30 15:10:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import hashlib
 import re
 import os
 pattern = re.compile("([^.+](?=[^\s@]*@g(oogle)?mail\.com))")
-test_list =open('/content/sample_data/mystring.txt')
+test_list =open(config.get("path", "input),"r")
 unique_list=[]
 unique_list2=[]
 ch = '.'
+config = configparser.ConfigParser()
+config.read("config.ini")
+settingscfg = config["path"]
 
 #select from .txt only gmail/googlemail email addresses and remove all dots 
 
@@ -20,22 +23,22 @@ for line in test_list:
               list_of_str = [elem.replace(ch,'')for elem in unique_list]
               without_space = ''.join(list_of_str)
 
-              with open('/content/sample_data/mystring2.txt','w+') as f:
+              with open(config.get("path", "temp"),'w+') as f:
                f.write(without_space) 
 #remove all Google domains from selected email adresses 
 
-with open(r'/content/sample_data/mystring2.txt', 'r') as infile, \
-     open(r'/content/sample_data/mystring3.txt', 'w') as outfile:
-    data = infile.read()
-    data = data.replace("@googlemailcom", "@gmailcom")
-    data = data.replace("@gmailcom", "")
+with open(config.get("path", "temp"),'r')  as infile, \
+      open(config.get("path", "temp1"),'w+') as outfile:
+      data = infile.read()
+      data = data.replace("@googlemailcom", "@gmailcom")
+      data = data.replace("@gmailcom", "")
 
-    outfile.write(data) 
+      outfile.write(data) 
 
 #find duplicated usernames 
 
-output_file_path = "/content/sample_data/mystring4.txt"
-input_file_path = "/content/sample_data/mystring3.txt"
+output_file_path = (config.get("path", "temp2"),'r') 
+input_file_path = (config.get("path", "temp1"),'r') 
 
 completed_lines_hash = set()
 
@@ -54,26 +57,26 @@ for line in open(input_file_path, "r"):
 output_file.close()
 
 #print usernames with Google alias abuse 
-final_print = open('/content/sample_data/mystring4.txt')
-if os.path.getsize('/content/sample_data/mystring4.txt') > 0:
+final_print = open(config.get("path", "temp2"),'r') 
+if os.path.getsize(config.get("path", "temp2"),'r') ) > 0:
 
   s1= 'username:'+''.join(final_print.read())
-  s2='I found these usernames abusing Gmail alias using DOT technique:\n'
+  s2="I found these usernames abusing Gmail alias using DOT technique:\n"
   s3 = "domain : @gmail.com OR googlemail.com"
   s4="\n these user has created more than one account using the same Google UUID"
 
   results = print(s2,s1,s3,s4)
-  with open('/content/sample_data/results.txt', 'w') as f:
+  with open((config.get("path", "output"), 'w+') as f:
     f.write(str.results)
     print(results)
-
-
 
 else:
 
   print('No gmail alias found!')
+            
 #clean-up of files 
 
-os.remove('/content/sample_data/mystring2.txt')
-os.remove('/content/sample_data/mystring3.txt')
-os.remove('/content/sample_data/mystring4.txt')
+os.remove(config.get("path", "temp"))
+os.remove(config.get("path", "temp1"))
+os.remove(config.get("path", "temp2"))
+
